03f5d723b821b1e7d84874ef189815b35ee447a7
Digitl Alchemyst <digitalalchemiststudios@gmail.com>
2024-03-17 22:44:46 -0500

    2.1.0 Prep

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    
-}
+  reactStrictMode: true,
+  swcMinify: true,
+  trailingSlash: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cloud.appwrite.io',
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
