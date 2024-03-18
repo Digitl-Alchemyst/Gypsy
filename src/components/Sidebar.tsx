5bc0876979a3a4d6218010273cfca8ff42d37c54
Digitl Alchemyst <digitalalchemiststudios@gmail.com>
2024-03-17 23:05:50 -0500

    .

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,45 +1,43 @@
 /* eslint-disable @next/next/no-img-element */
 'use client';
 
-import NewChat from './NewChat'
-import { useSession, signOut } from 'next-auth/react'
-import { EllipsisHorizontalIcon } from '@heroicons/react/24/outline'
+import NewChat from './NewChat';
+import { useSession, signOut } from 'next-auth/react';
+import { EllipsisHorizontalIcon } from '@heroicons/react/24/outline';
 
 function Sidebar() {
   const { data: session } = useSession();
 
-  return ( 
-    <div className='p-2 flex flex-col h-screen'>
-        <div className='flex-1'>            
-            <div>
-              
-              <NewChat />
+  return (
+    <div className='flex h-screen flex-col p-2'>
+      <div className='flex-1'>
+        <div>
+          <NewChat />
 
-              <div>
-                {/* Model Selection */}
-              </div>
+          <div>{/* Model Selection */}</div>
 
-                {/* Map through the ChatRows */}
-
-            </div>
+          {/* Map through the ChatRows */}
         </div>
-        {session && (
-          <div 
-            className='flex items-center p-2 gap-5 justify-between hover:bg-slate-700 rounded-md hover:shadow-md hover:border hover:border-slate-600/50 hover:cursor-pointer'
-            onClick={() => signOut()}
-          >
-
-            <div className='flex gap-5 items-center'>
-              <img src={session.user?.image!} alt='user' className='rounded-full h-12 w-12 cursor-pointer hover:opacity-50' />   
-              <span className='text-slate-300 text-lg font-semibold justify-self-start'>
-                  {session.user?.name!}
-              </span>
-            </div>
-
-            <EllipsisHorizontalIcon className='text-slate-300 h-8 w-8 justify-self-end' />              
-
+      </div>
+      {session && (
+        <div
+          className='flex items-center justify-between gap-5 rounded-md p-2 hover:cursor-pointer hover:border hover:border-slate-600/50 hover:bg-slate-700 hover:shadow-md'
+          onClick={() => signOut()}
+        >
+          <div className='flex items-center gap-5'>
+            <img
+              src={session.user?.image!}
+              alt='user'
+              className='h-12 w-12 cursor-pointer rounded-full hover:opacity-50'
+            />
+            <span className='justify-self-start text-lg font-semibold text-slate-300'>
+              {session.user?.name!}
+            </span>
           </div>
-        )}
+
+          <EllipsisHorizontalIcon className='h-8 w-8 justify-self-end text-slate-300' />
+        </div>
+      )}
     </div>
   );
 }
